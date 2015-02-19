5659855843777ad225558fcb30c21e1d04c7d0dd
herereadthis <herereadthis@gmail.com>
2015-02-19 03:09:20 -0500

    initialize stuff

---
+++ Blink/Blink.ino
@@ -0,0 +1,54 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(13, HIGH);
+    // wait
+    delay(200);
+    // turn the LED off by making the voltage LOW
+    digitalWrite(13, LOW);
+    // wait    
+    delay(200);            
+    digitalWrite(13, HIGH);
+    delay(200);            
+    digitalWrite(13, LOW); 
+    delay(200);            
+    digitalWrite(13, HIGH);
+    delay(200);            
+    digitalWrite(13, LOW); 
+    delay(400);
+    digitalWrite(13, HIGH);
+    // wait
+    delay(600);
+    digitalWrite(13, LOW);
+    delay(200);            
+    digitalWrite(13, HIGH);
+    delay(600);            
+    digitalWrite(13, LOW); 
+    delay(200);            
+    digitalWrite(13, HIGH);
+    delay(600);            
+    digitalWrite(13, LOW); 
+    delay(400);          
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # arduino
+
+
+### definitions
+
+
+#### Components
+
+- **Transducer** - components that change one type of energy into electrical energy, or vice versa.
+  - **Sensor** - things that convert other forms of energy into electrical energy.
+  - **Actuator** - things that chance electrical energy into other forms of energy.
+
+#### Electrical circuits
+
+- **Current** - the amount of electrical charge flowing past a specific point in your circuit, measured in amps (**A**)
+- **Voltage** - the difference in energy between one point in a circuit and another, measured in volts (**V**)
+- **Resistance** - how much a component resists the flow of electrical energy, measured in ohms (**Ω**)
